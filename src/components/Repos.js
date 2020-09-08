fd6938a34160c6f7b5736962bd77d535be58f9a7
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-08 09:28:13 +0200

    Calculate Most used Language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,7 +3,31 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  // console.log(repos);// Vemos que funciona
+  const chartData = [
+    {
+      label: 'Html',
+      value: '13',
+    },
+    {
+      label: 'Css',
+      value: '80',
+    },
+    {
+      label: 'Javascript',
+      value: '25',
+    },
+  ];
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={chartData}></Pie3D>
+        {/* <ExampleChart data={chartData}></ExampleChart>; */}
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
