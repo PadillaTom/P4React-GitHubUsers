d476ff045a524ed5f5c455e950cfa04d47ae70a6
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-08 08:19:22 +0200

    first component

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,22 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+// Provider
+const GithubProvider = ({ children }) => {
+  // Set the state and return a useState(Data, Function to setState)
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+// GithubContext --> Pasamos el Context dentro del provider y los children.
+// IMPORTANTE --> Value = {}
+export { GithubContext, GithubProvider };
+// Luego vamos a Index.js and Import GithubProvider
+// then --> Wrap the APP between <GithubProvider> APP </GithubProvider>
